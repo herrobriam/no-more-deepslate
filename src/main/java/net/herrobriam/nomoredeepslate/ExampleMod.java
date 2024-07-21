7dd304d3772f44431841d9d86db7b883d32def8c
herrobriam <131239470+herrobriam@users.noreply.github.com>
2024-07-21 13:52:38 +0100

    Initial forge commit

---
+++ src/main/java/net/herrobriam/nomoredeepslate/ExampleMod.java
@@ -0,0 +1,37 @@
+package net.herrobriam.nomoredeepslate;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ExampleMod.MODID)
+public class ExampleMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "nomoredeepslate";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+        LOGGER.info("Removing Deepslate... (I hate Forge)");
+    }
+}
